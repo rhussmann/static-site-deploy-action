51fc4ca8a207eff53346076d9f5880dca5e1dd7c
Ricky Hussmann <ricky.hussmann@gmail.com>
2021-07-25 21:14:52 -0400

    Trying to use local `string.starts`

--- custom.lua
+++ custom.lua
@@ -1,5 +1,6 @@
 local scheme = kong.request.get_scheme()
-if scheme == "http" and not string.starts(kong.request.get_path(), "/.well-known/acme-challenge/") then
+local string_starts = string.starts
+if scheme == "http" and not string_starts(kong.request.get_path(), "/.well-known/acme-challenge/") then
   local host = kong.request.get_host()
   local query = kong.request.get_path_with_query()
   local url = "https://" .. host ..query
